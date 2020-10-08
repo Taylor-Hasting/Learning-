a216ea6fc89e99ae36523c8cc828acf99fa51f64
robert <10thasting@gmail.com>
2020-10-07 21:38:27 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+name = input('what is your name')
+#ask the user to input their name and designate 'name' as a variable
+print ('Hi' + name + ', may I call you Mr. Dees?')
+# respond to the user telling them that you understand their name but wish to call them somethign different.
+response = input('')
+#allow the user to question the response but ignore it anyway.
+print ('Dees Nuts!!!!!')
+#joke goes here.
